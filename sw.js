3086e69fbed904feeaad87e73df52b06ea1d253e
ROCASS <rsanchezs151@gmail.com>
2020-08-26 10:37:08 -0500

    Modificacion iphone X +

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@ importScripts('js/sw-utils.js')
 
 
 
-const STATIC_CACHE      = 'static-v3';
-const DINAMYC_CACHE     = 'dinamic-v1';
+const STATIC_CACHE      = 'static-v4';
+const DINAMYC_CACHE     = 'dinamic-v2';
 const INMUTABLE_CACHE   = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -48,6 +48,10 @@ self.addEventListener( 'activate' , e =>  {
 
         keys.forEach( key => {
             if ( key !== STATIC_CACHE && key.includes('static')){
+                return caches.delete(key);
+            }
+
+            if ( key !== DINAMYC_CACHE && key.includes('dinamic')){
                 return caches.delete(key);
             }
 
