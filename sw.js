0c8c4741ea109f7df21d4bedf08d3a663b71c624
ROCASS <rsanchezs151@gmail.com>
2020-08-26 10:24:15 -0500

    Modificaciones para IOS

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ importScripts('js/sw-utils.js')
 
 
 
-const STATIC_CACHE      = 'static-v2';
+const STATIC_CACHE      = 'static-v3';
 const DINAMYC_CACHE     = 'dinamic-v1';
 const INMUTABLE_CACHE   = 'inmutable-v1';
 
